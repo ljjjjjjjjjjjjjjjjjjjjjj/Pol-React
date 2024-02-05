fe7f6fe0344d10b82990435f1f064152c32db97f
ljjjjjjjjjjjjjjjjjjjjjj <la.juozaityte@gmail.com>
2024-02-05 11:26:57 +0200

    "Testing main"

--- src/pages/page_administracija/AddAppointment.js
+++ src/pages/page_administracija/AddAppointment.js
@@ -108,9 +108,9 @@ const AddAppointment = () => {
 
 
   const handleEmployeeCategoryChange = (event) => {
-    console.log('Event:', event.target.value);
+    console.log('Event (main):', event.target.value);
     setAppEmployeeCategory("Seimos-medicina"); 
-    console.log("Category Selected:", appEmployeeCategory);
+    console.log("Category Selected (main):", appEmployeeCategory);
     setCategorySelected(true);
     fetchEmployeeByCategoryData();
 
