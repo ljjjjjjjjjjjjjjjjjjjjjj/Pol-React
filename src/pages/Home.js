ee7db6f8e8ff28dafc2d9ad68f3d6fda7efa092c
ljjjjjjjjjjjjjjjjjjjjjj <la.juozaityte@gmail.com>
2024-02-11 14:31:57 +0200

    Home page - updated content and formats

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,24 +10,42 @@ const Home = () => {
       
 
       <div className='home-imagecontainer-cover'>
-         <img className='home-imagecontainer-cover-image' src="./images/healthcare_800.png" alt={"healthcare"} /> 
-      </div>     
+         <img className='home-imagecontainer-cover-image' src="./images/healthcare_1600_longplus.png" alt={"healthcare"} /> 
+      </div>  
 
-      <p className="home"> template template template template template template template <br></br>
-         template template template template template template template <br></br>
-         template template template template template template template.</p>
+      <div className='home-box-1'> 
 
-      <p className="home"> template template template template template template template <br></br>
-         template template template template template template template <br></br>
-         template template template template template template template.</p>
+      <h3>Poliklinikoje teikiamos PSDF biudžeto lėšomis kompensuojamos (nemokamos) paslaugos</h3>
 
-      <p className="home"> template template template template template template template <br></br>
-         template template template template template template template <br></br>
-         template template template template template template template.</p>
+      <p className="home"> 
+         Vadovaujantis sutartimis su Valstybine ligonių kasa, šioje poliklinikoje teikiamos nemokamos, 
+         PSDF lėšomis kompensuojamos, paslaugos:</p>
 
-      <p className="home"> template template template template template template template <br></br>
-         template template template template template template template <br></br>
-         template template template template template template template.</p>
+
+      <ul>
+        <li> <strong>Pirminės ambulatorinės asmens sveikatos priežiūros paslaugos.&nbsp; </strong> 
+         Jos teikiamos visiems pacientams, kurie teisės aktų nustatyta tvarka yra apdrausti privalomuoju sveikatos draudimu 
+         ir yra prisirregistravusių prie poliklinikos gyventojų sąraše;</li>
+      </ul>  
+
+      <ul>
+        <li> <strong>Antrinės ambulatorinės sveikatos priežiūros paslaugos.&nbsp; </strong> 
+        Jos teikiamos pacientams, teisės aktų nustatyta tvarka, apdraustiems privalomuoju sveikatos draudimu 
+        bei turintiems gydytojo siuntimą konsultacijai 
+        (išskyrus antrinės sveikatos priežiūros paslaugas teikiantį gydytoją dermatologą ir odontologą);</li>
+      </ul>
+
+      <ul>
+        <li> <strong>Būtinoji medicinos pagalba.&nbsp; </strong> 
+        okia pagalba teikiama nemokamai visiems nuolatiniams gyventojams, neatsižvelgiant į tai, 
+        ar jie apdrausti privalomuoju sveikatos draudimu, ar ne. </li>
+      </ul>
+
+      
+
+      </div>  
+
+      
     </div>
   );
 };
