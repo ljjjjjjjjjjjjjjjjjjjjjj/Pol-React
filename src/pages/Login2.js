159cd47dcd816164e9b4acc6069baa1bd7ed2afd
ljjjjjjjjjjjjjjjjjjjjjj <la.juozaityte@gmail.com>
2024-02-13 09:12:17 +0200

    SECURITY - start of the security setup (React Hooks: JWT Authentication (without Redux)).  Signup and Login empty pages.

---
+++ src/pages/Login2.js
@@ -0,0 +1,36 @@
+
+
+import '../main/custom-bootstrap.css';
+import './formats/SignIn.css';
+import React, { useState } from 'react';
+
+
+
+
+
+const SignIn = () => {
+    
+
+
+
+
+  return (
+
+
+
+
+    <div className='administracija'>
+        
+      
+    </div>
+
+
+
+
+  );
+  };
+  
+  export default SignIn;
+
+
+
---
+++ src/pages/Signup1.js
@@ -0,0 +1,43 @@
+
+
+import '../main/custom-bootstrap.css';
+import './formats/SignIn.css';
+import React, { useState } from 'react';
+
+
+
+
+
+const Signup1 = () => {
+    
+
+  
+  
+
+
+
+
+  return (
+
+
+
+    <div className='administracija'>
+       
+
+
+
+
+
+    </div>
+
+
+
+
+
+  );
+  };
+  
+  export default Signup1;
+
+
+
